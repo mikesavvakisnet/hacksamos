fe218cf27a2d542342b6d8574c51e7e56a49a7c9
mikesavvakisnet <mike@savvakis.net>
2019-05-18 01:14:17 +0300

    mysql test query

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const pool = require('../config/database').pool;
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const data = await pool.query('SELECT NOW() as time_test');
+  const data = await pool.query('SELECT * from user');
 
-  res.render('index', { title: data[0].time_test });
+  res.send(JSON.parse(JSON.stringify(data))).status(200)
 
 });
 
