36be8f0468511190952c9999b21d447460b67993
mikesavvakisnet <mike@savvakis.net>
2019-05-18 04:25:17 +0300

    login/register functionality

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,79 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+var jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const pool = require('../config/database').pool;
 
 //Get all users
-router.get('/', async function(req, res, next) {
-  const data = await pool.query('SELECT * from USER');
+router.get('/', async function (req, res, next) {
+    const data = await pool.query('SELECT * from USER');
 
-  res.send(JSON.parse(JSON.stringify(data))).status(200)
+    // jwt.verify("tokenhere", process.env.JWT_SECRET, function(err, decoded) {
+    //     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    //
+    //     res.status(200).send(JSON.parse(JSON.stringify(decoded)));
+    // });
 
 });
 
 //Create user
-router.post('/', async function(req, res, next) {
-  //const data = await pool.query('INSERT TO USERS..');
+router.post('/register', async function (req, res, next) {
+    const {email, password, phone, role} = req.body;
+    const userExist = await pool.query('select email from user where email = ?', [email]);
 
-  res.send(JSON.parse(JSON.stringify(data))).status(200)
+    if (userExist.length === 0) {
+        const hashsalt = await bcrypt.hash(password, 10);
+        const data = await pool.query(`insert into user(email,password,phone,role) values (?,?,?,?)`, [email, hashsalt, phone, role]);
+        res.send(
+            {
+                "message": "Success"
+            }
+        ).status(200)
+    } else {
+        res.send(
+            {
+                "message": "Problem"
+            }
+        ).status(200)
+    }
+});
+
+//Login user
+router.post('/login', async function (req, res, next) {
+
+    const {email, password} = req.body;
+    const userExist = await pool.query('select email from user where email = ?', [email]);
+
+    if (userExist.length !== 0) {
+        const data = await pool.query(`select * from user where email = ?`, [email]);
+        if(await bcrypt.compare(password, data[0].password)){
+            var token = jwt.sign({ id: data[0].id }, process.env.JWT_SECRET, {
+                expiresIn: 86400 // expires in 24 hours
+            });
+            res.send(
+                {
+                    "message": "Success",
+                    "token": token
+                }
+            ).status(200)
+
+        }else{
+            res.send(
+                {
+                    "message": "Problem"
+                }
+            ).status(200)
+        }
+
+    } else {
+        res.send(
+            {
+                "message": "Problem"
+            }
+        ).status(200)
+    }
 
 });
 
